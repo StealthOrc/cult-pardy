1b9ff2c81fa9c2087b76f5270106a0b1af10ca68
StealthOrc <nitram1999@hotmail.de>
2024-04-20 15:15:07 +0200

    server: fixed /file/ call to actually use <applicationdir>/www/ for files!

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,41 +1,35 @@
 mod custom_ws;
 
+use actix::{Actor, StreamHandler};
+use actix_files::NamedFile;
+use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{HttpRequest, HttpResponse};
+use actix_web_actors::ws;
+use futures::task::waker;
 use std::any::Any;
+use std::env;
 use std::net::SocketAddr;
 use std::path::PathBuf;
-use actix_web::{get, web, App, HttpServer, Responder};
-use actix_files::NamedFile;
-use actix_web::{
-    HttpRequest, HttpResponse,
-};
-use actix::{Actor, StreamHandler};
-use actix_web_actors::ws;
 
 use anyhow::{Context, Result};
 use serde::de::Error;
 
-
 #[actix_web::main]
-async fn main() -> Result<()>{
+async fn main() -> Result<()> {
     let addr = "127.0.0.1";
     let port = 8081;
     let addr = format!("{}:{}", addr, port);
-    let addr = addr.parse::<SocketAddr>()
+    let addr = addr
+        .parse::<SocketAddr>()
         .context("Failed to parse address")?;
 
-    let server = HttpServer::new(|| {
-        App::new()
-            .service(start_ws)
-            .service(ping)
-            .service(index)
-    }).bind(addr)?
+    let server = HttpServer::new(|| App::new().service(start_ws).service(ping).service(index))
+        .bind(addr)?
         .run();
     println!("Started {} HttpServer! ", addr);
     server.await.expect("Server has crashed!");
     Ok(())
 }
-
-
 
 #[get("/ws")]
 async fn start_ws(req: HttpRequest, stream: web::Payload) -> HttpResponse {
@@ -47,12 +41,15 @@ async fn ping(_req: HttpRequest) -> HttpResponse {
     HttpResponse::Ok().body("true")
 }
 
-
 #[get("/file/{filename:.*}")]
 async fn index(req: HttpRequest) -> actix_web::Result<NamedFile> {
     let path: PathBuf = req.match_info().query("filename").parse().unwrap();
-    let mut wwwdir: String = String::from("\\www\\");
-    wwwdir.push_str(path.to_str().unwrap());
-    println!("{wwwdir}");
-    Ok(NamedFile::open(wwwdir)?)
+    let mut cexe = env::current_exe().unwrap();
+    cexe.pop();
+    cexe.push("www");
+    cexe.push(path);
+    let final_path = cexe.into_os_string().into_string().unwrap();
+
+    println!("path:{}", final_path);
+    Ok(NamedFile::open(final_path)?)
 }
