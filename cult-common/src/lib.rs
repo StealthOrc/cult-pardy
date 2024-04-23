f69663a77a973d97ab18c1f06f4905f05c36f514
Shiirroo <merken@hotmail.de>
2024-04-24 00:10:27 +0200

    Changed Error type to RequestError and changed Websocket impl, to accept Query Parms and not Headers

--- cult-common/src/lib.rs
+++ cult-common/src/lib.rs
@@ -9,22 +9,22 @@ use actix_web::cookie::time::format_description::FormatItem::Optional;
 use serde_json::Value;
 
 #[derive(Deserialize, Serialize)]
-enum HeaderErrorType{
+enum ErrorType{
     Missing,
     Empty,
     StringConversion,
 }
 
 #[derive(Deserialize, Serialize)]
-struct HeaderRequestError{
+struct RequestError{
     header_name: String,
-    error_type: HeaderErrorType
+    error_type: ErrorType
 }
 
-impl HeaderRequestError {
+impl RequestError {
 
-    fn new(name: &str, error_type:HeaderErrorType) -> Self {
-       HeaderRequestError{
+    fn new(name: &str, error_type:ErrorType) -> Self {
+       RequestError{
             header_name: name.to_string(),
             error_type
         }
@@ -38,32 +38,35 @@ pub fn get_internal_server_error_json(body:Value) -> HttpResponse {
 }
 
 
-pub fn extract_value<'a >(req: &'a HttpRequest, key: &str) -> Option<&'a str> {
+pub fn extract_value(req: &HttpRequest, key: &str) -> Result<String, HttpResponse>{
     let query_string = req.query_string();
+    if query_string.is_empty() {
+        return Err(HttpResponse::InternalServerError().json( RequestError::new(key, ErrorType::Empty)));
+    }
     for pair in query_string.split('&') {
         let mut parts = pair.split('=');
         if let Some(k) = parts.next() {
             if k == key {
                 if let Some(v) = parts.next() {
-                    return Some(v);
+                    return Ok(v.to_string());
                 }
             }
         }
     }
-    None
+    return Err(HttpResponse::InternalServerError().json( RequestError::new(key, ErrorType::Missing)));
 }
 
 
 pub fn extract_header_string(req: &HttpRequest, header_name: &str) -> Result<String, HttpResponse> {
     match req.headers().get(header_name) {
-        None => Err(HttpResponse::InternalServerError().json( HeaderRequestError::new(header_name, HeaderErrorType::Missing))),
+        None => Err(HttpResponse::InternalServerError().json( RequestError::new(header_name, ErrorType::Missing))),
         Some(value) => {
             if value.is_empty() {
-                Err(HttpResponse::InternalServerError().json( HeaderRequestError::new(header_name, HeaderErrorType::Empty)))
+                Err(HttpResponse::InternalServerError().json( RequestError::new(header_name,ErrorType::Empty)))
             } else {
                 match value.to_str() {
                     Ok(text) => Ok(text.to_string()),
-                    Err(_) => Err(HttpResponse::InternalServerError().json( HeaderRequestError::new(header_name, HeaderErrorType::StringConversion))),
+                    Err(_) => Err(HttpResponse::InternalServerError().json( RequestError::new(header_name, ErrorType::StringConversion))),
                 }
             }
         }
--- server/src/ws.rs
+++ server/src/ws.rs
@@ -4,18 +4,19 @@ use actix::Addr;
 use actix_web::{HttpRequest, HttpResponse, web};
 use actix_web_actors::ws;
 use serde_json::json;
-use cult_common::{extract_header_string, get_internal_server_error_json};
+use cult_common::{extract_header_string, extract_value, get_internal_server_error_json};
 use crate::server;
 use crate::session::WsSession;
 
 pub async fn start_ws(req: HttpRequest, stream: web::Payload,  srv: web::Data<Addr<server::GameServer>>) -> Result<HttpResponse, actix_web::Error> {
     //TODO MAKE MATCHES GREAT AGAIN!
-    let session_token = match extract_header_string(&req, "session-token") {
+
+    let session_token = match extract_value(&req, "session-token") {
         Ok(data) => data,
         Err(error) => return Ok(error),
     };
 
-    let lobby_id = match extract_header_string(&req, "lobby-id") {
+    let lobby_id = match extract_value(&req, "lobby-id") {
         Ok(data) => data,
         Err(error) => return Ok(error),
     };
