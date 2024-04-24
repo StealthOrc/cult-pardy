763dcd50a33fde0d11ae44aa2f5570dab2ca6f43
StealthOrc <nitram1999@hotmail.de>
2024-04-24 22:54:24 +0200

    merged server changes. introduced parse_addr_str(). app: add websocket try #1
    mal schauen was wird - hoffentlich geht nix kaputt.

---
+++ app/src/websocket.rs
@@ -0,0 +1,46 @@
+use futures::{channel::mpsc::Sender, future, SinkExt, StreamExt};
+use gloo_net::websocket::{futures::WebSocket, Message};
+use wasm_bindgen_futures::spawn_local;
+
+pub struct WebsocketService {
+    pub send_tunnel: Sender<String>,
+}
+impl WebsocketService {
+    // add code here
+    pub fn new(addr: &str) -> Self {
+        let mut ws = WebSocket::open(format!("ws://{}/ws", addr).as_str()).unwrap();
+        let (mut write, mut read) = ws.split();
+
+        let (mut tunnel_send, mut tunnel_receive) = futures::channel::mpsc::channel::<String>(1000);
+
+        spawn_local(async move {
+            while let Some(s) = tunnel_receive.next().await {
+                println!("message from channel {}", s);
+                write.send(Message::Text(s)).await.unwrap();
+            }
+        });
+
+        spawn_local(async move {
+            while let Some(msg) = read.next().await {
+                match msg {
+                    Ok(Message::Text(data)) => {
+                        println!("from websocket {}", data);
+                    }
+                    Ok(Message::Bytes(b)) => {
+                        let decoded = std::str::from_utf8(&b);
+                        if let Ok(val) = decoded {
+                            println!("from websocket (bin){}", val);
+                        }
+                    }
+                    Err(e) => {
+                        println!("ws error:{}", e);
+                    }
+                }
+            }
+        });
+
+        Self {
+            send_tunnel: tunnel_send,
+        }
+    }
+}
--- cult-common/src/lib.rs
+++ cult-common/src/lib.rs
@@ -1,47 +1,44 @@
-
-use actix_web::{HttpRequest, HttpResponse};
+use actix_web::cookie::time::format_description::FormatItem::Optional;
 use actix_web::http::header::ToStrError;
 use actix_web::web::{Json, JsonConfig};
-use serde::{Deserialize, Serialize};
+use actix_web::{HttpRequest, HttpResponse};
 use core::option::Option;
-use std::io::BufRead;
-use actix_web::cookie::time::format_description::FormatItem::Optional;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use std::io::BufRead;
+use std::net::SocketAddr;
 
 #[derive(Deserialize, Serialize)]
-enum ErrorType{
+enum ErrorType {
     Missing,
     Empty,
     StringConversion,
 }
 
 #[derive(Deserialize, Serialize)]
-struct RequestError{
+struct RequestError {
     header_name: String,
-    error_type: ErrorType
+    error_type: ErrorType,
 }
 
 impl RequestError {
-
-    fn new(name: &str, error_type:ErrorType) -> Self {
-       RequestError{
+    fn new(name: &str, error_type: ErrorType) -> Self {
+        RequestError {
             header_name: name.to_string(),
-            error_type
+            error_type,
         }
     }
-
-
-
 }
-pub fn get_internal_server_error_json(body:Value) -> HttpResponse {
+pub fn get_internal_server_error_json(body: Value) -> HttpResponse {
     HttpResponse::InternalServerError().json(body)
 }
 
-
-pub fn extract_value(req: &HttpRequest, key: &str) -> Result<String, HttpResponse>{
+pub fn extract_value(req: &HttpRequest, key: &str) -> Result<String, HttpResponse> {
     let query_string = req.query_string();
     if query_string.is_empty() {
-        return Err(HttpResponse::InternalServerError().json( RequestError::new(key, ErrorType::Empty)));
+        return Err(
+            HttpResponse::InternalServerError().json(RequestError::new(key, ErrorType::Empty))
+        );
     }
     for pair in query_string.split('&') {
         let mut parts = pair.split('=');
@@ -53,22 +50,34 @@ pub fn extract_value(req: &HttpRequest, key: &str) -> Result<String, HttpRespons
             }
         }
     }
-    return Err(HttpResponse::InternalServerError().json( RequestError::new(key, ErrorType::Missing)));
+    return Err(
+        HttpResponse::InternalServerError().json(RequestError::new(key, ErrorType::Missing))
+    );
 }
-
 
 pub fn extract_header_string(req: &HttpRequest, header_name: &str) -> Result<String, HttpResponse> {
     match req.headers().get(header_name) {
-        None => Err(HttpResponse::InternalServerError().json( RequestError::new(header_name, ErrorType::Missing))),
+        None => Err(HttpResponse::InternalServerError()
+            .json(RequestError::new(header_name, ErrorType::Missing))),
         Some(value) => {
             if value.is_empty() {
-                Err(HttpResponse::InternalServerError().json( RequestError::new(header_name,ErrorType::Empty)))
+                Err(HttpResponse::InternalServerError()
+                    .json(RequestError::new(header_name, ErrorType::Empty)))
             } else {
                 match value.to_str() {
                     Ok(text) => Ok(text.to_string()),
-                    Err(_) => Err(HttpResponse::InternalServerError().json( RequestError::new(header_name, ErrorType::StringConversion))),
+                    Err(_) => Err(HttpResponse::InternalServerError()
+                        .json(RequestError::new(header_name, ErrorType::StringConversion))),
                 }
             }
         }
     }
+}
+
+pub fn parse_addr_str(domain: &str, port: usize) -> SocketAddr {
+    let addr = "127.0.0.1";
+    let port = 8081;
+    let addr = format!("{}:{}", addr, port);
+    let addr = addr.parse::<SocketAddr>().expect("Failed to parse address");
+    addr
 }
