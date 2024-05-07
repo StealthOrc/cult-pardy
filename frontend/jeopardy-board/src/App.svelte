8cbb31c2ad565f2e1e81d9827727ce81224535de
StealthOrc <nitram1999@hotmail.de>
2024-05-07 18:19:04 +0200

    removed svelte mockup frontend

--- frontend/jeopardy-board/src/App.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-    import JeopardyBoard from './components/JeopardyBoard.svelte';
-    document.documentElement.style.overflow = 'hidden'; // Disable scrolling
-</script>
-<JeopardyBoard />
