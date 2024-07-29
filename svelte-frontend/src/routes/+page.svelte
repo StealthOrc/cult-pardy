bfe4a526c4edc74a3fe836d94a48eda7ee855e76
Shiirroo <merken@hotmail.de>
2024-07-30 00:49:00 +0200

    Updated PlayerIcon & Added Default

--- svelte-frontend/src/routes/+page.svelte
+++ svelte-frontend/src/routes/+page.svelte
@@ -7,33 +7,33 @@
 	import PlayerIcon from "./PlayerIcon.svelte";
     let lobbyid = '';
 
-    const cookies: cookies = getCookies();
-    let discord_user: DiscordUser;
+    let cookies: cookies = getCookies();
+    let discord_user: DiscordUser | null = null;
 
-    onMount( async () => {
-        console.log(`api/discord_session?user-session-id=${cookies.userSessionId.id}&session-token=${cookies.sessionToken.id}`);
-        //TODO: proxy to localhost:8000 needs be defined in config rather then here!
+    onMount(async () => {
         let response = await fetch(`api/discord_session?user-session-id=${cookies.userSessionId.id}&session-token=${cookies.sessionToken.id}`)
-        console.log(response);
         if (response.ok) {
             try {
                 discord_user = await response.json();
             } catch (e) {
                 console.error(e);
             }
+        } else {
+            console.error("Failed to fetch discord session");
         }
     })
+
+
 </script>
 <div class=" h-dvh w-dvw flex flex-col items-center justify-center gap-2">
     <input bind:value={lobbyid} class="border-2 border-blue-600 placeholder-blue-600 p-2 rounded m-2" type="text" name="lobby-id" id="lobby-id" placeholder="Lobby ID"/>
     <button on:click={() => window.location.href = `${base}/game/${lobbyid}`} class="bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 transition-all duration-200">Join Game</button>
     <button on:click={() => window.location.href = `${base}/game/main`} class="bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 transition-all duration-200">Join /main/ Game</button>
-     {#if discord_user != null}
-            <PlayerIcon {discord_user} />
-        {:else}
-        <a id="discord_login" href="discord" class="flex flex-row bg-discord-blue focus:outline-none focus:ring-2 transition-all duration-200 text-white font-bold py-2 px-4 rounded-lg">
+        <PlayerIcon {discord_user}/>
+        {#if !discord_user}
+            <a id="discord_login" href="discord" class="flex flex-row bg-discord-blue focus:outline-none focus:ring-2 transition-all duration-200 text-white font-bold py-2 px-4 rounded-lg">
                 <svg class="mr-2 self-center" aria-hidden="true" role="img" width="30" height="30" fill="none" viewBox="0 0 24 24"><path fill="currentColor" d="M19.73 4.87a18.2 18.2 0 0 0-4.6-1.44c-.21.4-.4.8-.58 1.21-1.69-.25-3.4-.25-5.1 0-.18-.41-.37-.82-.59-1.2-1.6.27-3.14.75-4.6 1.43A19.04 19.04 0 0 0 .96 17.7a18.43 18.43 0 0 0 5.63 2.87c.46-.62.86-1.28 1.2-1.98-.65-.25-1.29-.55-1.9-.92.17-.12.32-.24.47-.37 3.58 1.7 7.7 1.7 11.28 0l.46.37c-.6.36-1.25.67-1.9.92.35.7.75 1.35 1.2 1.98 2.03-.63 3.94-1.6 5.64-2.87.47-4.87-.78-9.09-3.3-12.83ZM8.3 15.12c-1.1 0-2-1.02-2-2.27 0-1.24.88-2.26 2-2.26s2.02 1.02 2 2.26c0 1.25-.89 2.27-2 2.27Zm7.4 0c-1.1 0-2-1.02-2-2.27 0-1.24.88-2.26 2-2.26s2.02 1.02 2 2.26c0 1.25-.88 2.27-2 2.27Z"></path></svg>
-            <p class="self-center">Login with Discord</p>
-        </a>
-    {/if}
+                <p class="self-center">Login with Discord</p>
+            </a>
+        {/if}
 </div>
--- svelte-frontend/src/routes/PlayerIcon.svelte
+++ svelte-frontend/src/routes/PlayerIcon.svelte
@@ -4,7 +4,7 @@
 	import { onMount } from 'svelte';
 	import { on } from 'svelte/events';
 
-    export let discord_user: DiscordUser;
+    export let discord_user: DiscordUser | null;
     const cookies: cookies = getCookies();
     let isAdmin : ApiResponse = {success: false};
 
@@ -17,10 +17,22 @@
             } catch (e) {
                 console.error(e);
             }
-        }
+        } 
     })
 
+
+    function getUserName() {
+        if (!discord_user) {
+            return cookies.userSessionId.id;
+        }
+        return discord_user.username
+    }
+
     function getAvatar() {
+        if (!discord_user) {
+            return "https://cdn-icons-png.flaticon.com/512/149/149071.png";
+        }
+
         let avatar_id = discord_user.avatar_id;
         let discord_id = discord_user.discord_id;
         return `https://cdn.discordapp.com/avatars/${discord_id}/${avatar_id}.png?size=640`;
@@ -31,14 +43,14 @@
     <div class="relative bg-gray-700 p-4 rounded-lg flex items-center space-x-4 shadow-lg">
         <!-- Shadow Overlay -->
         <div class="absolute inset-0 bg-gray-900 opacity-50 rounded-lg z-[-1]"></div>
-
+        {#key discord_user}
         <img src="{getAvatar()}" alt="Avatar" class="player-avatar w-16 h-16 rounded-full">
-        <div class="text-white text-lg flex flex-col items-start">
-            {#if isAdmin}
-                <span class="text-red-500 text-sm font-bold">[ADMIN]</span>
+        <div class="text-white text-lg flex flex-col items-center">
+            {#if isAdmin.success}
+                <span class="text-red-500 text-sm font-bold mb-1">[ADMIN]</span>
             {/if}
-            <p>{discord_user.username}</p>
+            <p class="text-center">{getUserName()}</p>
         </div>
+        {/key}
     </div>
 </div>
-
